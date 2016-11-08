6f27488822dcde61ec2c091a5502d7881007e659
DongyeBaima <919833563@qq.com>
2016-11-08 09:58:21 +0800

    Initial commit

---
+++ src/main/java/simpleDemo/TestPojo.java
@@ -0,0 +1,15 @@
+package simpleDemo;
+
+/**
+ * 描述 : 测试的pojo对象
+ * 版权 : Copyright-(c) 2016
+ * 公司 : Thinkive
+ *
+ * @author 王嵊俊
+ * @version 2016-10-18 17:34
+ */
+public class TestPojo {
+    public void printMessage() {
+        System.out.println("    printMessage");
+    }
+}
---
+++ src/test/java/simpleDemo/TestPojoTest.java
@@ -0,0 +1,14 @@
+package simpleDemo;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by SHENG on 2016/10/18.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring/spring-quartz.xml")
+public class TestPojoTest {
+
+}
